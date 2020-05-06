d563f22de5fbedfe2f9285fcf77829172af75ca8
bazunaka <bazunaka32@gmail.com>
2020-05-06 22:46:07 +0300

    new select from database

--- reg_Mail/mainwindow.cpp
+++ reg_Mail/mainwindow.cpp
@@ -2,34 +2,77 @@
 
 MainWidget::MainWidget(QWidget *parent) : QWidget(parent)
 {
-    tab             = new QTabWidget(this);
-    mnu_bar         = new QMenuBar(this);
-    pmnu1           = new QMenu("это меню 1");
-    pmnu2           = new QMenu("это меню 2");
-    verticalLayout  = new QVBoxLayout(this);
+    tab                   = new QTabWidget(this);
+    mnu_bar           = new QMenuBar(this);
+    pmnu1              = new QMenu("это меню 1");
+    pmnu2              = new QMenu("это меню 2");
+    verticalLayout   = new QVBoxLayout(this);
+    st_bar              = new QStatusBar(this);
+    tbl1                   = new QTableView;
+    tbl2                   = new QTableView;
 
     mnu_bar->addMenu(pmnu1);
     mnu_bar->addMenu(pmnu2);
 
-    tab->addTab(new QTableWidget, "название таба1");
-    tab->addTab(new QLabel("тут виджет2"), "название таба2");
-
     verticalLayout->addWidget(mnu_bar);
     verticalLayout->addWidget(tab);
+    verticalLayout->addWidget(st_bar);
 
-    setMinimumSize(650, 400);
-    resize(650, 400);
+    setMinimumSize(850, 450);
+    resize(850, 450);
     setWindowTitle("Учет электронных сообщений ЭП АСЗИ Цитрин");
 
-    add_tabs();
-    //QString db_driver = settings->value("db_connect/db_driver").toString();
-    //qDebug() << db_driver;
-}
-
-void MainWidget::add_tabs()
-{
     settings = new QSettings("settings.ini", QSettings::IniFormat);
-    settings->setValue("asd/asd", "qwe");
+
+    QString db_driver        = settings->value("db_connect/db_driver").toString();
+    QString db_drv_string  = settings->value("db_connect/db_drv_string").toString();
+    QString db_host           = settings->value("db_connect/db_host").toString();
+    QString db_name         = settings->value("db_connect/db_name").toString();
+    QString db_user           = settings->value("db_connect/db_user").toString();
+    QString db_password   = settings->value("db_connect/db_password").toString();
+
+    db = QSqlDatabase::addDatabase(db_driver);
+    db.setHostName(db_host);
+    db.setDatabaseName("DRIVER={" + db_drv_string + "};DATABASE=" + db_name + ";");
+    db.setUserName(db_user);
+    db.setPassword(db_password);
+
+    if (db.open())
+        {
+            st_bar->showMessage("Подключение успешно!");
+        } else {
+            st_bar->showMessage("Ошибка подключения!"); //реализовать вывод самой ошибки
+            qDebug() << db.lastError();
+        }
+
+    QStringList lst = db.tables();
+    foreach (QString str, lst)
+    {
+        qDebug() << str;
+    }
+    qDebug() << db_driver;
+
+    sqtbl1 = new QSqlTableModel(0, db);
+    sqtbl2 = new QSqlTableModel(0, db);
+
+    sqtbl1->setTable("send_mail");
+    sqtbl1->select();
+    sqtbl1->removeColumn(0);
+    sqtbl2->setTable("received_mail");
+    sqtbl2->select();
+    sqtbl2->removeColumn(0);
+
+
+    tbl1->setModel(sqtbl1);
+    tbl1->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    tbl2->setModel(sqtbl2);
+    tbl2->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    tab->addTab(tbl1, "название таба1");
+    tab->addTab(tbl2, "название таба2");
+
+    QModelIndexList index = tbl1->selectionModel()->selectedIndexes();
+    qDebug() << sqtbl1->select() << index;
 }
 
 MainWidget::~MainWidget()
--- reg_Mail/mainwindow.h
+++ reg_Mail/mainwindow.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <QtWidgets>
 #include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQueryModel>
+#include <QSqlTableModel>
 
 class MainWidget : public QWidget
 {
@@ -14,13 +18,26 @@ public:
     ~MainWidget();
 
 private:
-    QTabWidget  *tab;
-    QMenuBar    *mnu_bar;
-    QMenu       *pmnu1;
-    QMenu       *pmnu2;
-    QVBoxLayout *verticalLayout;
-    QSettings   *settings;
+    QTabWidget      *tab;
+    QTableView       *tbl1;
+    QTableView       *tbl2;
+    QMenuBar         *mnu_bar;
+    QMenu              *pmnu1;
+    QMenu              *pmnu2;
+    QVBoxLayout   *verticalLayout;
+    QSettings          *settings;
+    QStatusBar        *st_bar;
+    QSqlDatabase      db;  
+    QSqlTableModel  *sqtbl1;
+    QSqlTableModel  *sqtbl2;
+    QSqlQueryModel model1;
+    QSqlQueryModel model2;
 
-    void add_tabs();
+    QString *db_driver;
+    QString *db_drv_string;
+    QString *db_host;
+    QString *db_name;
+    QString *db_user;
+    QString *db_password;
 };
 #endif // MAINWINDOW_H
